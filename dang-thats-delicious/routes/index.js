f262d57c6b1ae667ce807156534cd9a07faa2a7a
Dru Kelly <drukelly@gmail.com>
2017-09-18 10:29:45 -0700

    Initial commit

---
+++ dang-thats-delicious/routes/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
+const { catchErrors } = require('../handlers/errorHandlers');
+
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
+router.get('/add', storeController.addStore);
+
+router.post('/add',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.createStore)
+);
+
+router.post('/add/:id',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.updateStore)
+);
+
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
+router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
+
+router.get('/tags', catchErrors(storeController.getStoresByTag));
+router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+
+// 1. validate registration data
+// 2. register the user
+// 3. log them in once registered
+router.post('/register', userController.validateRegister);
+
+
+module.exports = router;
