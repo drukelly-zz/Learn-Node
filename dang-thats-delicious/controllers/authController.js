b432ac4e5a280c23f2d8d247c1e51f8322347d2b
Dru Kelly <drukelly@gmail.com>
2017-09-23 14:06:51 -0700

    onto module 9

--- dang-thats-delicious/controllers/authController.js
+++ dang-thats-delicious/controllers/authController.js
@@ -2,6 +2,7 @@ const passport = require('passport');
 const crypto = require('crypto');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
+const promisify = require('es6-promisify');
 
 exports.login = passport.authenticate('local', {
   failureRedirect: '/login',
@@ -26,7 +27,7 @@ exports.isLoggedIn = (req, res, next) => {
   res.redirect('/login');
 };
 
-exports.forgot = async(req, res) => {
+exports.forgot = async (req, res) => {
   // 1. See if a user with that email exists
   const user = await User.findOne({ email: req.body.email });
   if (!user) {
@@ -35,7 +36,7 @@ exports.forgot = async(req, res) => {
   }
   // 2. Set reset tokens and expiry on their account
   user.resetPasswordToken = crypto.randomBytes(20).toString('hex');
-  user.resetPasswordExpires = Date.now() + 36 * 100000;
+  user.resetPasswordExpires = Date.now() + 3600000; // 1 hour from now
   await user.save();
   // 3. Send them an email with the token
   const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
@@ -66,3 +67,23 @@ exports.confirmedPasswords = (req, res, next) => {
   res.redirect('back');
 };
 
+exports.update = async (req, res) => {
+  const user = await User.findOne({
+    resetPasswordToken: req.params.token,
+    resetPasswordExpires: { $gt: Date.now() }
+  });
+
+  if (!user) {
+    req.flash('error', 'Password reset is invalid or has expired');
+    return res.redirect('/login');
+  }
+  
+  const setPassword = promisify(user.setPassword, user);
+  await setPassword(req.body.password);
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpires = undefined;
+  const updatedUser = await user.save();
+  await req.login(updatedUser);
+  req.flash('success', 'Nice! Your password has been reset! You are now logged in!');
+  res.redirect('/');
+};
